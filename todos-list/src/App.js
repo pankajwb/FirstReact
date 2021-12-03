9a89e2120e2cd790faa2e96f388ebe1f1595887b
Pankaj <pankajwb@gmail.com>
2021-12-03 15:51:55 +0530

    Adding basic react app files and bootstrap css-js

---
+++ todos-list/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  let myVar = 123;
+  return (
+    <div className="App">
+      <h2>My App Works</h2>
+      <p>Hello World!</p>
+    </div>
+  );
+}
+
+export default App;
